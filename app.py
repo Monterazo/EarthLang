a189b2d795b3eae56c508590b30d14e35a9791d4
Lucas Monterazo <lsm6@cin.ufpe.br>
2023-07-31 20:42:37 -0300

    Add chain to shape the results

--- app.py
+++ app.py
@@ -5,22 +5,23 @@ from apikey import apikey
 os.environ['OPENAI_API_KEY'] = apikey
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
-
-#App framwork 
+#App framework 
 sl.title('Ways to save Earth 🌎')
 prompt = sl.text_input("Ask me:")
 
 #Prompt templates
-title_template = PromptTemplate(
+tips_template = PromptTemplate(
   input_variables=['topic'],   
-  template='write me one way to save earth about: {topic}'                        
+  template='Write me one tip of how to save Earth in the field of: {topic}'                        
                                 
 )
 
 #LLM - definição da LLM usada
 llm= OpenAI (temperature=0.9) #Diz quão criativo será nosso modelo
+tips_chain = LLMChain (llm=llm, prompt=tips_template, verbose= True)
 
 if prompt: #Função que passa o prompt para a LLM
-  response = llm(prompt)
+  response = tips_chain.run(prompt)
   sl.write(response) #Mostra a resposta da LLM
